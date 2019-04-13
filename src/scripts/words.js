248dfa13006f518a9308ab720de1313698e39844
nbstsh <nbstsh@gmail.com>
2019-04-14 02:34:00 +0900

    Create script to collect words and save themt to db

---
+++ src/scripts/words.js
@@ -0,0 +1,31 @@
+const collectWords = require('../utils/words-collect')
+const { Word, validateWord } = require('../models/word')
+const logger = require('../log/logger')
+
+require('../startup/error')
+require('../startup/config')
+require('../startup/db')
+
+const main = async () => {
+    logger.info('Start collecting-word process ...')
+    const words = await collectWords()
+
+    const saveWordPromises = words.map(({ display, hiragana }) => {
+        const startWith = hiragana[0]
+        const word = { startWith, display, hiragana }
+
+        const { error } = validateWord(word)
+        if (error) return null 
+
+        
+        return new Word(word).save().then(() => {
+            logger.info(`Successfully saved >>> { startWith: ${startWith}, display: ${display}, hiragana: ${hiragana}}`)
+        })
+    })
+
+    await Promise.all(saveWordPromises)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1))
